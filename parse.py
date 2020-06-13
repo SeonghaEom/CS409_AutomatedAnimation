1197dceea1e6671d8f7ce301aa7541a266b67573
seongha <doubleb@kaist.ac.kr>
2020-06-13 14:12:44 +0900

    add getCenter, getFormationChunk

--- parse.py
+++ parse.py
@@ -1,11 +1,10 @@
 import json
+import numpy as np
+import copy
+import pprint
+np.set_printoptions(threshold=np.inf)
 
 class Video():
-    # def __init__(self, frame, behavior, hmcnt):
-    #     frame = frame 
-    #     behavior = behavior 
-    #     hmcnt = hmcnt
-
     def __init__(self, name):
         with open(name) as json_file:
             json_string = json.load(json_file)
@@ -14,16 +13,11 @@ class Video():
 
         for i in range(len(json_data)):
             boxes = json_data[str(i)]
-            # if (len(boxes) == 0):
-            #     continue
             humans = []
             for box_ind in boxes:
                 each = Human(boxes[box_ind]['id'], i, boxes[box_ind]['pose_pos'])
                 # print(boxes[box_ind]['pose_pos'])
                 humans.append(each)
-                # print(each)
-            # print(frame.__dict__)
-            # frames.append(Frame(i, humans))
             Frame(i, humans)
             # print(len(humans))
             if (hum_cnt < len(boxes)):
@@ -32,6 +26,8 @@ class Video():
         self.frame = Frame
         self.behavior = Behavior
         self.hmcnt = hum_cnt
+        self.center = {}
+        self.formationChunk = {}
 
     def Track(self, bh, start, end):
         trk = []
@@ -41,6 +37,78 @@ class Video():
                 trk.append(json.dumps({'frame_id': human.frame_id, 'human_id': human.id, 'pose_pos': li}, sort_keys=True))
         return trk
 
+    def getCenter(self, frameid):
+        if not self.center:
+            #self.center is empty, initlize it
+            for each in self.frame.instances:
+                if (len(each.humans) == self.hmcnt):
+                    print ("frame no ", each.id)
+                    cores = []
+                    for hum in each.humans:
+                        # calculate core pos
+                        a = np.array(hum.pose_pos)
+                        core = np.mean(a, axis=0)
+                        # print (core)
+                        cores.append((core, hum.id))
+                    cores.sort(key = lambda element: element[0][0])
+                    self.center[each.id] = cores[self.hmcnt//2][1]
+                    # print(cores)
+            # print(self.center)
+
+        #self.center is calculated
+        if (frameid in self.center.keys()):
+            return self.center[frameid]
+        else:
+            return -1
+
+    def getFormationChunk(self):
+        if not self.formationChunk:
+            # formation chunk is empty
+            previous_form =[]
+            formation = []
+            first = 0
+            # last = 0
+            for each in self.frame.instances:
+                previous_form = copy.copy(formation)
+                if (len(each.humans) == self.hmcnt):
+                    cores = []
+                    formation = []
+                    for hum in each.humans:
+                        # calculate core pos
+                        a = np.array(hum.pose_pos)
+                        core = np.mean(a, axis=0)
+                        # print (core)
+                        cores.append((core, hum.id))
+                    cores.sort(key = lambda element: element[0][0])
+                    for ea in cores:
+                        formation.append(ea[1])
+                    if str(previous_form) != str(formation):
+                        self.formationChunk[(first, each.id)] = previous_form
+                        first = each.id
+                # else:
+                #     # eac.humans not fully recognized
+                #     if (previous_form):
+                #         self.formationChunk[(first, each.id)] = previous_form
+                #     # previous_form = []
+                #     formation = []
+                #     first = each.id
+        return self.formationChunk
+    
+    def getFeetMov(self):
+        feet = {}
+        i = 0
+        for i in range(self.hmcnt):
+            feet[i+1] = {"left": [], "right": []}
+        for each in self.frame.instances:
+            for hum in each.humans:
+                # print (hum.id)
+                # print (hum.pose_pos[15])
+                # print (hum.pose_pos[16])
+                feet[hum.id]["left"].append(hum.pose_pos[15])
+                feet[hum.id]["right"].append(hum.pose_pos[16])
+        print (np.gradient(feet[1]["left"], axis = 1))
+        
+    
 class Behavior():
     instances = []
     def __init__(self, id, akpnt):
@@ -59,6 +127,8 @@ class Human():
         self.id = id
         self.frame_id = frame_id
         self.pose_pos = pose_pos #[[x,y],..,]
+        self.is_center = False
+
 
 class Frame():
     instances = []
@@ -77,115 +147,5 @@ class Frame():
     def getLength(cls):
         return len(cls.instances)
     
-
-
-# def makeVideo(name):
-#     with open(name) as json_file:
-#         json_string = json.load(json_file)
-#     json_data = json.loads(json_string)
-#     hum_cnt = 0
-
-#     for i in range(len(json_data)):
-#         boxes = json_data[str(i)]
-#         # if (len(boxes) == 0):
-#         #     continue
-#         humans = []
-#         for box_ind in boxes:
-#             each = Human(boxes[box_ind]['id'], i, boxes[box_ind]['pose_pos'])
-#             # print(boxes[box_ind]['pose_pos'])
-#             humans.append(each)
-#             # print(each)
-#         # print(frame.__dict__)
-#         # frames.append(Frame(i, humans))
-#         Frame(i, humans)
-#         # print(len(humans))
-#         if (hum_cnt < len(boxes)):
-#             hum_cnt = len(boxes)
-#     return Video(Frame, Behavior, hum_cnt)
-
-# vd = makeVideo('../(G)I-DLE-01_matching.json')
-# print (vd.frame.get(10).__dict__)
-
-
-
-# print("generating json deserialization... ")
-# with open('../(G)I-DLE-01_matching.json') as json_file:
-#     json_string = json.load(json_file)
-
-# json_data = json.loads(json_string)
-
-# print("frame index ranges  from 0 to ", len(json_data))
-# frame_ind = input("enter frame index: ")
-# print("frame index ranges  from 1 to ", len(json_data[frame_ind]))
-# box_ind =  input("enter frame index: ")
-# print(json_data[frame_ind][box_ind])
-# print (json_data['0']['1'])
-
-# print ("frame length: ", len(json_data))
-# # print (json_data['99']['1'])
-# for i in range(len(json_data)):
-#     each  = json_data[str(i)]
-#     for j in json_data[str(i)]:
-#         print (each[j])
-
-frames = []
-behaviors = []
-hum_cnt = 0
-
-# for i in range(len(json_data)):
-#     boxes = json_data[str(i)]
-#     # if (len(boxes) == 0):
-#     #     continue
-#     humans = []
-#     for box_ind in boxes:
-#         each = Human(boxes[box_ind]['id'], i, boxes[box_ind]['pose_pos'])
-#         # print(boxes[box_ind]['pose_pos'])
-#         humans.append(each)
-#         # print(each)
-#     # print(frame.__dict__)
-#     frames.append(Frame(i, humans))
-#     # print(len(humans))
-#     if (hum_cnt < len(boxes)):
-#         hum_cnt = len(boxes)
-
-# Video(frames, behaviors, hum_cnt)
-# print("number of humans in this video: ", hum_cnt)
-# print(Frame.instances[0].__dict__)
-# print(Frame.instances[0].id)
-# print(Frame.instances[0].humans[0].frame_id)
-# print(Frame.instances[0].humans[0].pose_pos)
-# print (Frame.get(9).__dict__)
-
-""" for example, behavior id 0 with anker point 9, 10 """
-behaviors.append(Behavior(0, [9,10]))
-
-""" for example, track motion for frame id 9~19, Behavior id 0 """
-# for frame_id in range(9, 20):
-#     for human in Frame.get(frame_id).humans:
-#         print(human.frame_id)
-#         print(human.id)
-#         # print(human.pose_pos)
-#         li = [ human.pose_pos[anker] for anker in Behavior.get(0).akpnt]
-#         print (li)
-
-# for anker in Behavior.get(0).akpnt:
-#     # print(anker)
-#     human.pose_pos[anker]
-
-# def Track(bh, start, end):
-#     trk = []
-#     for frame_id in range(start, end):
-#         for human in Frame.get(frame_id).humans:
-#             li = [ human.pose_pos[anker] for anker in Behavior.get(bh).akpnt]
-#             trk.append(json.dumps({'frame_id': human.frame_id, 'human_id': human.id, 'pose_pos': li}, sort_keys=True))
-#     return trk
-
-
-# print(Track(0, 9, 11))
-
-# def getAnkpntInBehavior(idx):
-#     return Behavior.get(idx).akpnt
-
-# print (getAnkpntInBehavior(0))
 
 
--- test.py
+++ test.py
@@ -2,22 +2,68 @@ from parse import Behavior
 from parse import Human
 from parse import Frame
 from parse import Video
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
 
 """ initialize behavior """
+""" TODO: index uniquely assign """
 clap = Behavior(0, [9,10])
 raiseArm = Behavior(1, [0,1,4,7])
+
 
 print (clap.akpnt)
 
 """ initialize video """
 video = Video('../(G)I-DLE-01_matching.json')
+i = 0
+# for i in range(0, 1500):
+#     print (video.getCenter(i))
+# print (video.getFormationChunk())
+print(video.getFeetMov())
 
-print ("There are ", video.hmcnt, "people in this video")
-print ("There are ", len(video.frame.get(9).humans), "people in this frame")
-print ("There are ", video.frame.getLength(), "frames")
+# print ("There are ", video.hmcnt, "people in this video")
+# print ("There are ", len(video.frame.get(9).humans), "people in this frame")
+# print ("There are ", video.frame.getLength(), "frames")
 
 """ track frame 99 ~ 101 by clap behavior """
-print(video.Track(clap, 99, 101))
+# print(video.Track(clap, 99, 101))
+# plot = []
+# x = []
+# y = []
+# y2 = []
+# previous_form =[]
+# formation = []
+# for each in video.frame.instances:
+#     previous_form = copy.copy(formation)
+#     if (len(each.humans) == video.hmcnt):
+#         print ("frame no ", each.id)
+#         if (each.id > 5000):
+#             x.append(each.id)
+#         cores = []
+#         formation = []
+#         for hum in each.humans:
+#             # calculate core pos
+#             a = np.array(hum.pose_pos)
+#             core = np.mean(a, axis=0)
+#             # print (core)
+#             cores.append((core, hum.id))
+#         cores.sort(key = lambda element: element[0][0])
+#         for ea in cores:
+#             formation.append(ea[1])
+#         if (each.id > 5000):
+#             y.append(cores[video.hmcnt//2][1])
+#         if (str(previous_form) == str(formation) and each.id > 5000):
+#             y2.append(1)
+#         elif (each.id > 5000):
+#             y2.append(0)
+#         print(cores)
+
+# plt.scatter(x,y)
+# plt.show()
+
+
+
 
 
 
